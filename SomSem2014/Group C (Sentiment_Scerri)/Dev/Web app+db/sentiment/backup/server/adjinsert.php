64bcf456ad70bda22d5dbf73f80bbfad711475f5
mh khodashahi <mh.khodashahi@gmail.com>
2014-10-01 00:58:37 +0200

    Revert "Add Codes"
    This reverts commit b39a628f2053e39ef891f129b37d86fcf53beca1.

--- SomSem2014/Group C (Sentiment_Scerri)/Dev/Web app+db/sentiment/backup/server/adjinsert.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'DBconnect.php';
-$link=dbconnect();
-echo $query="INSERT INTO `sentiment`.`adjectives` ( `name`, `sense`) VALUES ( 'آرامشی', 'negative')";
-$result=mysql_query($query);
-?>
